4ba18189c97c78927d5a0096c091d4652f22f114
Erick Alexander Barrera Ochoa <ebarrera-2019263@kinal.edu.gt>
2023-01-19 16:54:08 -0600

    se agrego la documentacion a las clases

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,13 @@
 public class Main {
+
+    /*
+     * @author Erick Barrera
+     * @author Sergio Palacios
+     * @author Jose Gramajo
+
+     * @date 19- 01 - 2023
+     * Clase principal para poder interactuar con la radio
+     */
+
+
 }
--- src/Radio.java
+++ src/Radio.java
@@ -1,2 +1,10 @@
 public class Radio {
+    /*
+     * @author Erick Barrera
+     * @author Sergio Palacios
+     * @author Jose Gramajo
+
+     * @date 19- 01 - 2023
+     * Clase principal para poder interactuar con la radio
+     */
 }
