9377c04b37dfcab8c44481023e6000bc55034c03
ebarrera2019263 <ebarrera-2019263@kinal.edu.gt>
2023-01-19 18:21:20 -0600

    se agrego la interfaz de la radio

--- src/IRadio.java
+++ src/IRadio.java
@@ -1,2 +1,34 @@
 public interface IRadio {
+    public void TurnOn();
+
+    public void TurnOff();
+
+    public boolean OnOff();
+
+    public void setFrequence(String frequency) throws Exception;
+
+    public String getFrequence();
+
+    public void Change();
+
+    public void Back();
+
+    public double getStationActualAM();
+
+    public double getStationActualFM();
+
+    public double getStationActualAM(double ActualStation);
+
+    public double getStationActualFM(int ActualStation);
+
+    public void saveStationFM(double actualStation, int slot);
+
+    public void saveStationAM(int actualStation, int slot);
+
+    public double getFMSlot(int slot);
+
+    public int getAMSlot(int slot);
+
+
+
 }
