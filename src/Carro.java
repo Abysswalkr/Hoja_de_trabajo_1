645ffe8df0f7b0223e9af5256cfd529d21fab219
Jose Gramajo <gra22907@uvg.edu.gt>
2023-01-19 18:03:10 -0600

    Atributes cration

--- src/Carro.java
+++ src/Carro.java
@@ -1,12 +1,22 @@
-public class Carro {
+public class Carro implements IRadio {
     /*
      * @author Erick Barrera
      * @author Sergio Palacios
      * @author Jose Gramajo
      * @author Pablo Noack
-     *
 
      * @date 19- 01 - 2023
      * Clase principal para poder interactuar con la radio
      */
+
+    // attributes
+
+    private boolean turnOn;
+    private String frequence;
+    private double FMstation;
+    private int AMstation;
+    private double[] saveFMstation;
+    private int[] saveAMstation;
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ public class Main {
      * @author Erick Barrera
      * @author Sergio Palacios
      * @author Jose Gramajo
-     * @author Pablo Noack
 
      * @date 19- 01 - 2023
      * Clase principal para poder interactuar con la radio
